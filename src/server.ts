6fe2e34995ad19461276e884846b2077271a7799
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-03-09 11:51:47 +0300

    09.03.2023

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import express from "express";
 import http from "http";
 import mongoose from "mongoose";
 import { config } from "./config/config";
+import Logging from "./library/Logging";
 
 const router = express();
 mongoose
   .connect(config.mongo.url, { retryWrites: true, w: "majority" })
   .then(() => {
-    console.log("Mongo connected successfully.");
+    // console.log("Mongo connected successfully.");
+    Logging.info("Mongo connected successfully.");
     // StartServer();
   })
-  .catch((error) => console.log(error));
+  .catch((error) => {
+    Logging.error(error);
+    Logging.error("Mongo connection failed.");
+    // console.log(error)
+  });
